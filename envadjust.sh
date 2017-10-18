fd0684f4642fb8fc6c2ff52658e7972683eee847
Erik Wierschke <erik.wierschke@plus3it.com>
2017-10-18 14:11:30 -0400

    initial script

---
+++ envadjust.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#--pull variables from files--
+export ENVIRNAME=$(cat /usr/local/bin/envirname)
+export PWMURL=$(cat /usr/local/bin/pwmurl)
+export OSTURL=$(cat /usr/local/bin/osturl)
+export OSTEMAIL=$(cat /usr/local/bin/ostemail)
+export EMAILFROM=$(cat /usr/local/bin/emailfrom)
+export SVCACCTUPN=$(cat /usr/local/bin/svcacctupn)
+export SVCACCTPASS=$(cat /usr/local/bin/svcacctpass)
+export LDAPHOSTNAME=$(cat /usr/local/bin/ldaphostname)
+export ADMINEMAILTO=$(cat /usr/local/bin/adminemailto)
+export OUPATH=$(cat /usr/local/bin/oupath)
+
+#--envir adjust--
+sed -i.bak "s|_ENVIRNAME_|${ENVIRNAME}|g" /usr/local/bin/ad-password-expire-notify/user_email_inlined.tpl
+sed -i.bak "s|_ENVIRNAME_|${ENVIRNAME}|g" /usr/local/bin/ad-password-expire-notify/admin_email_inlined.tpl
+sed -i.bak "s|_PWMURL_|${PWMURL}|g" /usr/local/bin/ad-password-expire-notify/user_email_inlined.tpl
+sed -i.bak "s|_OSTURL_|${OSTURL}|g" /usr/local/bin/ad-password-expire-notify/user_email_inlined.tpl
+sed -i.bak "s|_OSTEMAIL_|${OSTEMAIL}|g" /usr/local/bin/ad-password-expire-notify/user_email_inlined.tpl
+
+#--php adjust--
+sed -i.bak "s|;date.timezone =|date.timezone = America/New_York|" /etc/php.ini
+sed -i.bak "s|sendmail_path = .*|sendmail_path = /usr/sbin/sendmail -t -i -f \"${EMAILFROM}\"|" /etc/php.ini
+
+#--check_expire adjust--
+sed -i.bak '0,/\$scriptPath.*/ s/\$scriptPath.*/\$scriptPath = "\/usr\/local\/bin\/ad-password-expire-notify\/";/' /usr/local/bin/ad-password-expire-notify/check_expire.php
+sed -i.bak "0,/\$ldapupn.*/ s/\$ldapupn.*/\$ldapupn = \"${SVCACCTUPN}\";/" /usr/local/bin/ad-password-expire-notify/check_expire.php
+sed -i.bak "0,/\$ldappass.*/ s/\$ldappass.*/\$ldappass = \"${SVCACCTPASS}\";/" /usr/local/bin/ad-password-expire-notify/check_expire.php
+##todo-need to control for $ or other res chars in password
+sed -i.bak "0,/\$ldaphost.*/ s/\$ldaphost.*/\$ldaphost = \"${LDAPHOSTNAME}\";/" /usr/local/bin/ad-password-expire-notify/check_expire.php
+sed -i.bak "0,/\$adminemailto.*/ s/\$adminemailto.*/\$adminemailto = \"${ADMINEMAILTO}\";/" /usr/local/bin/ad-password-expire-notify/check_expire.php
+sed -i.bak "0,/\$useremailheader \.= \"From.*/ s/\$useremailheader \.= \"From.*/\$useremailheader \.= \"From: Password Expiration Warning <${EMAILFROM}>\" . \"\\\r\\\n\";/" /usr/local/bin/ad-password-expire-notify/check_expire.php
+sed -i.bak "0,/\$adminemailheader \.= \"From.*/ s/\$adminemailheader \.= \"From.*/\$adminemailheader \.= \"From: Password Expiration Warning <${EMAILFROM}>\" . \"\\\r\\\n\";/" /usr/local/bin/ad-password-expire-notify/check_expire.php
